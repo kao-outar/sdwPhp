94d0ca1a973543464afba4868c26e0ff08421275
kao-outar <kaoutar.arare@gmail.com>
2024-01-30 10:39:50 +0100

    Afficher le contenu des variables

--- exemple1.php
+++ exemple1.php
@@ -15,12 +15,26 @@
             $fruit[0] = "fraise";
             $fruit[1] = "banane";
             $fruit[2] = "abricot";
-        ?>
-        <?php
+        
+        
             $fruit = Array();
             $fruit['le_meilleur'] = "fraise";
             $fruit['le_prefere_de_Julien'] = "banane";
             $fruit['mon_prefere'] = "abricot";
+        
+        
+            $nom = "LA GLOBULE";
+            echo 'Bonjour ';
+            echo $nom;
+            echo ' !';
+        
+
+            $date_du_jour = date ("d-m-Y");
+            $heure_courante = date ("H:i");
+            echo 'Nous sommes le : ';
+            echo $date_du_jour;
+            echo ' Et il est : ';
+            echo $heure_courante;
         ?>
     </body>
 </html>
