3e55aa3718ee87b453b706097b0c069db01bcf1d
kao-outar <kaoutar.arare@gmail.com>
2024-01-30 10:26:13 +0100

    ajouter un commentaire

--- exemple1.php
+++ exemple1.php
@@ -5,6 +5,10 @@
     <body>
         <?php
             echo "c'est un script PHP";
+            // ceci est un commentaire sur une seule ligne
+            /* ceci est
+            un commentaire
+            sur plusieures lignes */
         ?>
     </body>
 </html>
