e48e628bba3c8cc347564015fed38408fd10b053
kao-outar <kaoutar.arare@gmail.com>
2024-01-30 10:31:35 +0100

    ajouter des arrays

--- exemple1.php
+++ exemple1.php
@@ -10,5 +10,17 @@
             un commentaire
             sur plusieures lignes */
         ?>
+        <?php
+            $fruit = Array();
+            $fruit[0] = "fraise";
+            $fruit[1] = "banane";
+            $fruit[2] = "abricot";
+        ?>
+        <?php
+            $fruit = Array();
+            $fruit['le_meilleur'] = "fraise";
+            $fruit['le_prefere_de_Julien'] = "banane";
+            $fruit['mon_prefere'] = "abricot";
+        ?>
     </body>
 </html>
