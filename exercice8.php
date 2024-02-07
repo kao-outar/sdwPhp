00d9f66391462ab8682c1d258a937f8ee97e9a8b
kao-outar <kaoutar.arare@gmail.com>
2024-02-07 11:24:44 +0100

    exercice8 : les dates

---
+++ exercice8.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice sur la Gestion des Dates</title>
+</head>
+<body>
+
+<?php
+//date du jour au format "jour mois année"
+$date_du_jour = date("j F Y");
+
+// Affichage
+echo "Nous sommes le : $date_du_jour";
+?>
+
+</body>
+</html>
+    
